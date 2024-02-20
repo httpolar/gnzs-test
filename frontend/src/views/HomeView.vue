c1345e764cd3b5ad54060a32f2bb86cd10faf42c
httpolar <http@polar.moe>
2024-02-20 22:51:53 +0400

    feat: home page with buttons

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,25 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import Button from '@/components/Button.vue'
+import Container from '@/components/Container.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main class="main" :class="$style.homeContainer">
+    <Container>
+      <Button variant="blue">Hello</Button>
+      <Button variant="blue" loading>Hello</Button>
+      <Button variant="blue" disabled>Hello</Button>
+    </Container>
   </main>
 </template>
+
+<style lang="scss" scoped module>
+@import "@/assets/variables";
+
+.homeContainer {
+  display: flex;
+  flex-direction: column;
+  padding-top: 0.5rem;
+  padding-bottom: 0.5rem;
+}
+</style>
