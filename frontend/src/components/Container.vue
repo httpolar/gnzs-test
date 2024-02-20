9a045479fbd54fa8f09634cab3573e1a6e1a4870
httpolar <http@polar.moe>
2024-02-20 22:36:04 +0400

    feat: container component

---
+++ frontend/src/components/Container.vue
@@ -0,0 +1,17 @@
+<template>
+  <div :class="$style.container">
+    <slot/>
+  </div>
+</template>
+
+<style lang="scss" module scoped>
+@import "@/assets/variables";
+
+.container {
+  max-width: $max-content-width;
+  width: 100%;
+
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
