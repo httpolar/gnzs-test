37d400895c102844ec714ca55550c0989dafd822
httpolar <http@polar.moe>
2024-02-20 22:51:39 +0400

    feat: about page with select usage example

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,15 +1,25 @@
+<script setup lang="ts">
+import SelectRoot from "@/components/Select/SelectRoot.vue";
+import SelectTrigger from "@/components/Select/SelectTrigger.vue";
+import SelectOptions from "@/components/Select/SelectOptions.vue";
+import SelectOption from "@/components/Select/SelectOption.vue";
+import { ref } from "vue";
+import Container from '@/components/Container.vue'
+
+const selected = ref<string | null>(null);
+</script>
+
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <Container>
+    <SelectRoot v-model="selected" style="width: 200px">
+      <SelectTrigger placeholder="Choose something" />
+
+      <SelectOptions>
+        <SelectOption value="Anime">Anime</SelectOption>
+        <SelectOption value="Games">Games</SelectOption>
+      </SelectOptions>
+    </SelectRoot>
+  </Container>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
+
