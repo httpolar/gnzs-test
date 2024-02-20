17662179cb8e48e602476b72c2c11036e8b88955
httpolar <http@polar.moe>
2024-02-20 22:50:42 +0400

    feat: Header component

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header :class="$style.headerContainer">
+    <div :class="$style.headerWrapper">
+      <nav :class="$style.navContainer">
+        <RouterLink to="/" :class="$style.navLink" :active-class="$style.navLinkActive">
+          Главная
+        </RouterLink>
+        <RouterLink to="/about" :class="$style.navLink" :active-class="$style.navLinkActive">
+          About
+        </RouterLink>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<style module scoped lang="scss">
+@import "@/assets/variables";
+
+.headerContainer {
+  height: $header-height;
+  border-bottom-style: solid;
+  border-bottom-color: $border;
+  border-bottom-width: 1px;
+}
+
+.headerWrapper {
+  max-width: $max-content-width;
+  width: 100%;
+  height: 100%;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.navContainer {
+  display: flex;
+  height: 100%;
+  gap: 2rem;
+  justify-content: center;
+  align-items: center;
+}
+
+.navLink {
+  font-size: 0.875rem;
+  text-decoration: none;
+  color: black;
+  opacity: 0.7;
+}
+
+.navLinkActive {
+  opacity: 1 !important;
+}
+</style>
