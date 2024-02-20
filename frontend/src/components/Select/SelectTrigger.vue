35209e403505143f0b8e46b828739112c6cad38a
httpolar <http@polar.moe>
2024-02-20 22:38:23 +0400

    chore: remove tailwind comment from SelectTrigger

--- frontend/src/components/Select/SelectTrigger.vue
+++ frontend/src/components/Select/SelectTrigger.vue
@@ -44,10 +44,6 @@ function handleUnderlyingButtonClick() {
 <style lang="scss" scoped module>
 @import "@/assets/variables";
 
-/**
-flex h-9 items-center justify-between whitespace-nowrap rounded-md border border-input bg-transparent px-3 py-2 text-sm shadow-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-1 focus:ring-ring disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1 w-[180px]
- */
-
 .selectTriggerButton {
   display: inline-flex;
   justify-content: space-between;
