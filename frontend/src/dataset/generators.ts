3f9c8510fa30c5e7b39f19cff53f8084ee30bdd9
httpolar <http@polar.moe>
2024-02-21 21:32:28 +0400

    feat: added generators for random names and company names

---
+++ frontend/src/dataset/generators.ts
@@ -0,0 +1,25 @@
+import { FIRST_NAMES_F, FIRST_NAMES_M, LAST_NAMES_F, LAST_NAMES_M } from "@/dataset/names";
+import { takeRandomElement } from "@/lib/take-random-element";
+import { RANDOM_WORDS } from '@/dataset/words'
+
+export function generateRandomName() {
+  let firstName: string;
+  let lastName: string;
+
+  if (Math.random() < 0.5) {
+    firstName = takeRandomElement(FIRST_NAMES_F);
+    lastName = takeRandomElement(LAST_NAMES_F);
+  } else {
+    firstName = takeRandomElement(FIRST_NAMES_M);
+    lastName = takeRandomElement(LAST_NAMES_M);
+  }
+
+  return [firstName, lastName] as const;
+}
+
+export function generateCompanyName() {
+  const word = takeRandomElement(RANDOM_WORDS);
+  const form = takeRandomElement(["LLC", "GmbH", "Ltd."]);
+
+  return `${word} ${form}`
+}
